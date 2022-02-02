1a9703877f5cbfd5c9697414620376dba3fb009d
Dino-Martinez <>
2022-02-02 13:08:20 -0500

    Exported my functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const capitalize = str => {
+const capitalize = (str) => {
   const arr = str.split('')
   arr[0] = arr[0].toUpperCase()
   return arr.join('')
 }
 
-const lowercase = str => {
+const lowercase = (str) => {
   const arr = str.split('')
   arr[0] = arr[0].toLowerCase()
   return arr.join('')
 }
 
-const allCaps = str => str.toUpperCase()
+const allCaps = (str) => str.toUpperCase()
 
-const capitalizeWords = str => {
+const capitalizeWords = (str) => {
   const arr = str.split(' ')
   arr.forEach((word, index, arr) => {
     arr[index] = capitalize(word)
-  });
+  })
   return arr.join(' ')
 }
 
-const removeSpaces = str => {
+const removeSpaces = (str) => {
   str = str.trim()
   const arr = str.split(' ')
   str = arr.filter(word => word !== '').join(' ')
@@ -34,42 +34,42 @@ const replaceSpaces = (str, replacement) => {
   return arr.join(replacement)
 }
 
-const kebobCase = str => replaceSpaces(str, '-')
+const kebobCase = (str) => replaceSpaces(str, '-')
 
-const snakeCase = str => replaceSpaces(str, '_')
+const snakeCase = (str) => replaceSpaces(str, '_')
 
-const camelCase = str => {
+const camelCase = (str) => {
   str = removeSpaces(str)
   const arr = str.split(' ')
   arr.forEach((word, index, arr) => {
     arr[index] = capitalize(word)
-  });
+  })
   arr[0] = lowercase(arr[0])
   return arr.join('')
 }
 
-const shift = (str, numShifts=1) => {
+const shift = (str, numShifts = 1) => {
   numShifts *= -1
   const shifted = str.slice(numShifts)
   return shifted + str.slice(0, numShifts)
 }
 
-const makeHashTag = str => {
+const makeHashTag = (str) => {
   let arr = str.split(' ')
   if (arr.length > 3) {
-    arr = arr.sort((a,b) => b.length - a.length)
-    arr = arr.slice(0,3)
+    arr = arr.sort((a, b) => b.length - a.length)
+    arr = arr.slice(0, 3)
   }
 
   str = capitalizeWords(arr.join(' '))
   arr = str.split(' ')
   arr.forEach((word, index, arr) => {
     arr[index] = '#' + word
-  });
+  })
   return arr
 }
 
-const isEmpty = str => {
+const isEmpty = (str) => {
   str = str.trim()
   const arr = str.split('')
   let result = true
@@ -82,4 +82,18 @@ const isEmpty = str => {
   return result
 }
 
-console.log(isEmpty(``))
+
+module.exports = {
+  capitalize,
+  allCaps,
+  lowercase,
+  capitalizeWords,
+  removeSpaces,
+  replaceSpaces,
+  camelCase,
+  kebobCase,
+  snakeCase,
+  shift,
+  makeHashTag,
+  isEmpty
+}
